7926a2253ae103e3929186cbacae2133f6bd1989
SafetImamovic <safet.imamovic.22@size.ba>
2025-07-19 20:19:36 +0200

    Added cool font for Oxide

--- src/commands/oxide.rs
+++ src/commands/oxide.rs
@@ -1,4 +1,21 @@
 pub fn render_oxide()
 {
-        println!("Rendered!")
+        let oxide_string = r#"
+
+ ▄██████▄  ▀████    ▐████▀  ▄█  ████████▄     ▄████████ 
+███    ███   ███▌   ████▀  ███  ███   ▀███   ███    ███ 
+███    ███    ███  ▐███    ███▌ ███    ███   ███    █▀  
+███    ███    ▀███▄███▀    ███▌ ███    ███  ▄███▄▄▄     
+███    ███    ████▀██▄     ███▌ ███    ███ ▀▀███▀▀▀     
+███    ███   ▐███  ▀███    ███  ███    ███   ███    █▄  
+███    ███  ▄███     ███▄  ███  ███   ▄███   ███    ███ 
+ ▀██████▀  ████       ███▄ █▀   ████████▀    ██████████ 
+
+ Web 3D Render Engine built with wgpu and Rust. 
+(ASCII art generated @ https://www.patorjk.com/software/taag/
+[font: Delta Corps Priest 1])
+
+            "#;
+
+        println!("{oxide_string}")
 }
